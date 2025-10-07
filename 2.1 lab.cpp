e9805eab2a65fcabd4cc6a60b21015d40f4fc295
unknown <chuyamim57@gmail.com>
2025-10-07 03:49:44 +0300

    first master commit

---
+++ 2.1 lab.cpp
@@ -0,0 +1,25 @@
+// Lab_02.cpp
+// Івасів Вікторії
+// Лабораторна робота № 2.1
+// Лінійні програми.
+// Варіант 7
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	double Pi = 4 * atan(1.); // число пі
+	double a; // вхідний параметр
+	double z1; // результат обчислення 1-го виразу
+	double z2; // результат обчислення 2-го виразу
+	cout << "a = "; cin >> a;
+
+	z1 = cos(3.0 / 8.0 * Pi - a / 4.0) * cos(3.0 / 8.0 * Pi - a / 4.0) - cos(11.0 / 8.0 * Pi + a / 4.0) * cos(11.0 / 8.0 * Pi + a / 4.0);
+	z2 = sqrt(2.0) / 2.0 * sin((a * 1.0) / 2.0);
+	cout << endl;
+	cout << "z1 = " << z1 << endl;
+	cout << "z2 = " << z2 << endl;
+	cin.get();
+	return 0;
+}
