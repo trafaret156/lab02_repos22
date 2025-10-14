8ab573059bf631c1eae28cc1ffbd7f5539fc690e
trafaret156 <chuyamim57@gmail.com>
2025-10-14 05:03:50 +0300

    Add files via upload

---
+++ 4.4 lab.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+int main()
+{
+	double x, xp, xk, dx, R, y;
+	cout << "xp = "; cin >> xp;
+	cout << "xk = "; cin >> xk;
+	cout << "dx = "; cin >> dx;
+	cout << "R = "; cin >> R;
+	double k = (6 + R) / (9 - R);
+	double b = -R * (1 + k);
+	cout << fixed;
+	cout << "---------------------------" << endl;
+	cout << "|" << setw(5) << "x" << " |"
+		<< setw(7) << "y" << " |" << endl;
+	cout << "---------------------------" << endl;
+	x = xp;
+	while (x <= xk)
+	{
+		if (x <= -R)
+			y = R;
+		else
+			if (x < R)
+				y = -sqrt(R * R - x * x);
+			else
+				y = k * x + b;
+		cout << "|" << setw(7) << setprecision(2) << x
+			<< " |" << setw(10) << setprecision(3) << y
+			<< " |" << endl;
+		x += dx;
+	}
+	cout << "---------------------------" << endl;
+	return 0;
+}
